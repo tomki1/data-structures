e2d3fce47b312eb90b89e491387427fffe203f7f
Kimberly Tom <kimberlyjtom@gmail.com>
2023-07-11 13:11:21 -0700

    Update linkedList.js

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,37 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+
+    if (list.tail === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    if (list.head !== null) {
+      var removedHead = list.head.value;
+      list.head = list.head.next;
+      return removedHead;
+    }
+
   };
 
   list.contains = function(target) {
+    if (list.head !== null) {
+      var currentNode = list.head;
+      while (currentNode !== null) {
+        if (currentNode.value === target) {
+          return true;
+        }
+        currentNode = currentNode.next;
+      }
+    }
+    return false;
   };
 
   return list;
@@ -23,6 +48,7 @@ var Node = function(value) {
 
   return node;
 };
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
