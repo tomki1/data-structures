5fb7fcea61ea43a1b1e0166d028e036c67322ee0
Kimberly Tom <kimberlyjtom@gmail.com>
2023-07-11 13:10:08 -0700

    Update binarySearchTree.js

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,46 @@
 var BinarySearchTree = function(value) {
+  var binaryTree = Object.create(binaryTreePrototype);
+  binaryTree.value = value;
+  binaryTree.left = null;
+  binaryTree.right = null;
+  return binaryTree;
 };
 
+var binaryTreePrototype = {};
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+binaryTreePrototype.insert = function (val) {
+  if (val < this.value) {
+    if (this.left === null) {
+      this.left = BinarySearchTree(val);
+    } else {
+      this.left.insert(val);
+    }
+
+  } else if (val > this.value) {
+    if (this.right === null) {
+      this.right = BinarySearchTree(val);
+    } else {
+      this.right.insert(val);
+    }
+  }
+};
+
+binaryTreePrototype.contains = function(val) {
+  if (val === this.value) {
+    return true;
+  } else if (val < this.value) {
+    return !!(this.left && this.left.contains(val));
+  } else if (val > this.value) {
+    return !!(this.right && this.right.contains(val)); //return null
+  }
+};
+
+binaryTreePrototype.depthFirstLog = function(callBack) {
+  callBack(this.value);
+  if (this.left) {
+    this.left.depthFirstLog(callBack);
+  }
+  if (this.right) {
+    this.right.depthFirstLog(callBack);
+  }
+};
